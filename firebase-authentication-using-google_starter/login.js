5e442035acb9545acb6da6d86ef5a8fb794d663f
Peter McCallum <85507238+pmccallu@users.noreply.github.com>
2022-01-24 10:54:16 -0600

    Add files via upload

---
+++ firebase-authentication-using-google_starter/login.js
@@ -0,0 +1,97 @@
+(function () {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyALug6Vo8MMszq2x2wFV9MjFgZ5u1nO-5I",
+    authDomain: "week26-1.firebaseapp.com",
+    projectId: "week26-1",
+    storageBucket: "week26-1.appspot.com",
+    messagingSenderId: "773323709036",
+    appId: "1:773323709036:web:f8bfce49398d8dd96e5649",
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // get elements
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const login = document.getElementById("login");
+  const signup = document.getElementById("signup");
+  const logout = document.getElementById("logout");
+  const loggedInStatus = document.getElementById("loggedInStatus");
+  const googlelogin = document.getElementById("googlelogin");
+
+  //TODO: Add Google Sign in
+  googlelogin.addEventListener("click", (e) => {
+    console.log("google sign in clicked");
+    // TODO: Use firebase.auth.GoogleAuthProvider() to implement Google sign in
+    // Hint: the user email address is in the results user object: result.user.email
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        loggedInStatus.innerText = `You are logged in using the following email: ${result.user.email}`;
+        login.style.display = "none";
+        signup.style.display = "none";
+        email.style.display = "none";
+        password.style.display = "none";
+        googlelogin.style.display = "none";
+        logout.style.display = "none";
+      })
+      .catch(function (error) {
+        console.log(error.code);
+        console.log(error.message);
+      });
+  });
+
+  // login
+  login.addEventListener("click", (e) => {
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // signup
+  signup.addEventListener("click", (e) => {
+    // TODO: check for real email
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // logout
+  logout.addEventListener("click", (e) => {
+    firebase.auth().signOut();
+  });
+
+  // login state
+  firebase.auth().onAuthStateChanged((firebaseUser) => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      loggedInStatus.innerText = `You are logged in using the following email: ${result.user.email}`;
+      logout.style.display = "inline";
+      login.style.display = "none";
+      signup.style.display = "none";
+      email.style.display = "none";
+      password.style.display = "none";
+      googlelogin.style.display = "none";
+    } else {
+      console.log("User is not logged in");
+      loggedInStatus.innerText = "You are not yet logged in";
+      login.style.display = "inline";
+      signup.style.display = "inline";
+      email.style.display = "inline";
+      googlelogin.style.display = "inline";
+      password.style.display = "inline";
+      logout.style.display = "none";
+    }
+  });
+})();
